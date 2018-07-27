0ab0707e38478125af316d2b524accd83ffa5c22
Sam Freeside <snovvcrash@protonmail.com>
2018-07-27 23:33:06 +0300

    Minor edits

--- credentials.py
+++ credentials.py
@@ -3,23 +3,23 @@
 
 credentials = [
 	{
-		'consumer_key': ' ',
-		'consumer_secret': ' ',
-		'access_token_key': ' ',
-		'access_token_secret': ' '
+		'consumer_key': 'CONSUMER_KEY',
+		'consumer_secret': 'CONSUMER_SECRET',
+		'access_token_key': 'ACCESS_TOKEN_KEY',
+		'access_token_secret': 'ACCESS_TOKEN_SECRET'
 	},
 
 	#{
-	#	'consumer_key': ' ',
-	#	'consumer_secret': ' ',
-	#	'access_token_key': ' ',
-	#	'access_token_secret': ' '
+	#	'consumer_key': 'CONSUMER_KEY',
+	#	'consumer_secret': 'CONSUMER_SECRET',
+	#	'access_token_key': 'ACCESS_TOKEN_KEY',
+	#	'access_token_secret': 'ACCESS_TOKEN_SECRET'
 	#},
 
 	#{
-	#	'consumer_key': ' ',
-	#	'consumer_secret': ' ',
-	#	'access_token_key': ' ',
-	#	'access_token_secret': ' '
+	#	'consumer_key': 'CONSUMER_KEY',
+	#	'consumer_secret': 'CONSUMER_SECRET',
+	#	'access_token_key': 'ACCESS_TOKEN_KEY',
+	#	'access_token_secret': 'ACCESS_TOKEN_SECRET'
 	#},
 ]
