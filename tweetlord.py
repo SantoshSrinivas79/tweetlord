98d753dfdca3ad93050ff47af353f7c4c7f413ea
Sam Freeside <snovvcrash@protonmail.ch>
2018-08-28 17:52:13 +0300

    "python" → "py"

--- tweetlord.py
+++ tweetlord.py
@@ -341,7 +341,7 @@ def build_xlsx(dump, filename, username):
 	})
 
 	worksheet.write(0, 0, 'This document was generated with tweetl\U0001F451rd tool (by snovvcrash)', signature_fmt)  # 👑
-	worksheet.write(1, 0, 'at {} as: "python {}"'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), ' '.join(sys.argv)), signature_fmt)
+	worksheet.write(1, 0, 'at {} as: "py {}"'.format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), ' '.join(sys.argv)), signature_fmt)
 	worksheet.write(2, 0, 'v{}'.format(VERSION), signature_version_fmt)
 	worksheet.write(3, 0, '{}'.format(SITE), signature_site_fmt)
 
